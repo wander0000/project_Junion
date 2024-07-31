21363aff27198e3d3fa30becba6dcf133d12b433
sooyoonhee <ruin2150@gmail.com>
2024-07-31 12:56:16 +0900

    24.07.31 commit test 12:56

--- boot_junion/src/main/java/com/boot/Controller/ScrapController.java
+++ boot_junion/src/main/java/com/boot/Controller/ScrapController.java
@@ -27,6 +27,7 @@ public ResponseEntity<Boolean> comScrap(@RequestParam HashMap<String, String> pa
 	log.info("comScrap controller!!!");
 	log.info("comScrap controller param!!! "+param);
 	log.info("스크랩 컨트롤러 git 연동 확인용 ");
+	log.info("commit이 제대로 되었나요? ");
 	
 	boolean result = scrapService.comScrap(param);
 	return new ResponseEntity<Boolean>(result, HttpStatus.OK);
