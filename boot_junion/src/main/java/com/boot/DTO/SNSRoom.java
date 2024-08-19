5f22a55eb376c81a51b36153cb74a3a05ed2f278
dsafj112 <fgghk132@naver.com>
2024-08-19 10:59:04 +0900

    240819 SNS 채팅 목록 서버단 (나성엽)

--- boot_junion/src/main/java/com/boot/DTO/SNSRoom.java
+++ boot_junion/src/main/java/com/boot/DTO/SNSRoom.java
@@ -1,5 +1,7 @@
 package com.boot.DTO;
 
+import java.sql.Timestamp;
+
 import lombok.Data;
 
 @Data
@@ -7,4 +9,9 @@ public class SNSRoom {
     private int chatRoom_id;
     private String senderId;
     private String receiverId;
+    
+    private String user_name;
+    private String sender_id;
+    private String message;
+    private Timestamp timestamp;
 }
